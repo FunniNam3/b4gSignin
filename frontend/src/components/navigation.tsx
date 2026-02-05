3556a0a544f7d489b69c491f7a8cf2823d053806
FunniNam3 <nguyen.ev03@gmail.com>
2026-02-05 03:00:46 -0600

    working on nav but need to get rid of env

--- frontend/src/components/navigation.tsx
+++ frontend/src/components/navigation.tsx
@@ -5,8 +5,18 @@ export function Navigation() {
   const { user } = useAuth();
   return (
     <nav className="flex w-full items-center justify-between p-3 border-b">
-      <Link className="hover:scale-105" to="/">
-        <img src="logo.png" alt="" className="h-20" />
+      <Link className="group" to="/">
+        <h1 className="text-5xl font-[Modak] flex flex-row items-baseline">
+          B{/* Left side "uild" */}
+          <span className="inline-block origin-right scale-x-0 opacity-0 transition-all duration-500 delay-100 group-hover:scale-x-100 group-hover:opacity-100">
+            uild
+          </span>
+          <img src="logo 4.svg" alt="B4G Logo" className="h-13 mx-1" />G
+          {/* Right side "ood" */}
+          <span className="inline-block origin-left scale-x-0 opacity-0 transition-all duration-500 delay-200 group-hover:scale-x-100 group-hover:opacity-100">
+            ood
+          </span>
+        </h1>
       </Link>
 
       <div className="flex flex-row gap-2 text-2xl">
